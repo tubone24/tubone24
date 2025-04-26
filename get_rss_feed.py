02a52f3896c3b2ee1c1e387d606a3204877a601b
tubone(Yu Otsubo) <tubo.yyyuuu@gmail.com>
2025-04-27 03:12:36 +0900

    Update get_rss_feed.py

--- get_rss_feed.py
+++ get_rss_feed.py
@@ -3,23 +3,60 @@ import datetime
 import locale
 import feedparser
 
-URL = "https://blog.tubone-project24.xyz/rss.xml"
-
+RSS_FEEDS = {
+    "blog": "https://blog.tubone-project24.xyz/rss.xml",
+    "slides": "https://slide-tubone24.pages.dev/rss.xml" 
+}
 
 def get_rss(url):
-    locale.setlocale(locale.LC_TIME, 'en_US.UTF-8')
+    """指定したURLからRSSフィードを取得し、必要な情報を抽出します"""
+    try:
+        locale.setlocale(locale.LC_TIME, 'en_US.UTF-8')
+    except locale.Error:
+        pass
+    
     d = feedparser.parse(url)
-    return [{"title": entry["title"], "link": entry["link"], "date": datetime.datetime.strptime(entry["published"], "%a, %d %b %Y %H:%M:%S %Z").strftime("%Y-%m-%d")} for entry in d["entries"]]
+    results = []
+    
+    for entry in d.entries:
+        if 'published' in entry:
+            try:
+                date = datetime.datetime.strptime(entry.published, "%a, %d %b %Y %H:%M:%S %Z")
+            except ValueError:
+                try:
+                    date = datetime.datetime.strptime(entry.published, "%Y-%m-%dT%H:%M:%S%z")
+                except ValueError:
+                    date = datetime.datetime.now()
+        else:
+            date = datetime.datetime.now()
+        
+        formatted_date = date.strftime("%Y-%m-%d")
+        
+        results.append({
+            "title": entry.title,
+            "link": entry.link,
+            "date": formatted_date
+        })
+    
+    return results
 
-
-def generate_markdown(rss):
-    header = "<table><tr><td valign=\"top\" width=\"100%\">\n\n### Blog Ratest Posts\n\n"
-    contents = "\n\n".join(["- [" + x["title"] + "](" + x["link"] + ") - " + x["date"] for x in rss][0:5])
-    footer = "\n\n</td></tr></table>"
-    return header+contents+footer
-
+def generate_markdown(rss_dict):
+    """すべてのRSSフィードのマークダウンを生成します"""
+    header = "<table><tr><td valign=\"top\" width=\"100%\">\n\n"
+    content = ""
+    
+    for feed_title, feed_items in rss_dict.items():
+        content += f"### {feed_title.capitalize()} Latest Posts\n\n"
+        # 各フィードの最大5つの項目を一覧表示
+        for item in feed_items[:5]:
+            content += f"- [{item['title']}]({item['link']}) - {item['date']}\n"
+        content += "\n"
+    
+    footer = "</td></tr></table>"
+    return header + content + footer
 
 def paste_markdown(content):
+    """生成したマークダウンをREADME.mdファイルに挿入します"""
     with codecs.open("README.md", "r", "utf-8") as f:
         md = f.read()
         head = md.split("<!-- generate_markdown_start -->")[0]
@@ -27,8 +64,11 @@ def paste_markdown(content):
     with codecs.open("README.md", "w", "utf-8") as f:
         f.write(head + "<!-- generate_markdown_start -->\n\n" + content + "\n\n<!-- generate_markdown_end -->" + end)
 
-
 if __name__ == "__main__":
-    content = generate_markdown(get_rss(URL))
+    rss_items = {}
+    for feed_name, feed_url in RSS_FEEDS.items():
+        rss_items[feed_name] = get_rss(feed_url)
+    
+    content = generate_markdown(rss_items)
     print(content)
     paste_markdown(content)
